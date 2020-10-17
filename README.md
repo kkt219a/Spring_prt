e355641e8283acb0058ef63c35a8a555036a43e3
GYUTAEKIM <kkt219a@gmail.com>
2020-10-18 04:22:36 +0900

    controlloer build
    cont

---
+++ README.md
@@ -0,0 +1,2 @@
+# Base on Spring Boot
+# Space for study
--- src/main/java/springt/Springs2Application.java
+++ src/main/java/springt/Springs2Application.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springs2Application {
 
 	public static void main(String[] args) {
-		// 스트링부트 애플리케이션, 톰캣 웹서버를 내장하고 있어서
-		// 실행을 하게 된다.
-		// gradle 통하지 않게 하면 빠르게 된다?
+//		 톰캣 웹서버를 내장하고 있어서
+//		 저 함수 실행하면, 클래스 넣으면 위에 @통해 실행을 한다 띄우면서 톰캣 웹서버를 내장하고 있어서
+//		 자체적으로 스트링부트 애플리케이션이 올라온다
 		
 		SpringApplication.run(Springs2Application.class, args);
 	}
