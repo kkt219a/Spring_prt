575c5387b9e8872dd78cb7f92f9cb623436aa428
GYUTAEKIM <kkt219a@gmail.com>
2020-12-16 00:14:37 +0900

    mvc connect

---
+++ src/main/java/springt/controller/MemberForm.java
@@ -0,0 +1,9 @@
+package springt.controller;
+
+public class MemberForm {
+
+    private String name;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+}
