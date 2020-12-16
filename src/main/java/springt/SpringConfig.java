4821201ecab31116983bbc24fbb2c5fe0b6ce442
GYUTAEKIM <kkt219a@gmail.com>
2020-12-16 23:02:12 +0900

    db access study

--- src/main/java/springt/SpringConfig.java
+++ src/main/java/springt/SpringConfig.java
@@ -2,20 +2,30 @@ package springt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springt.repository.JdbcMemberRepository;
 import springt.repository.MemberRepository;
 import springt.repository.MemoryMemberRepository;
 import springt.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
